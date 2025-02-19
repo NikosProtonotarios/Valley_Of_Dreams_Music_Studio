667bedf81c5278daed5d885d20ce9c3457157bab
NikosProtonotarios <147170230+NikosProtonotarios@users.noreply.github.com>
2025-02-19 17:15:44 +0000

    create nav

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,24 @@ function App() {
 
   return (
     <div>
+      <nav className='navBar'>
+        <div className='navImageContainer'>
+          <div className='navImage'></div>
+        </div>
+        <div className='navLinks' style={{fontFamily: "oswald", fontSize: "23px"}}>
+          <p>pRog.R Studio</p>
+          <p>VoD MusicVerse</p>
+          <p>VoD Records</p>
+          <p>Contact</p>
+          <p>About</p>
+        </div>
+      </nav>
+      <section className='sectionContainer'>
 
+      </section>
+      <footer className='footerContainer'>
+        <p>2023 pRog.R Studio</p>
+      </footer>
     </div>
   )
 }
